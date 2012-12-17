746ed306127d18e8c7544482697aa978aa8d5856
Mick <mickfreeze@gmail.com>
2012-12-18 00:30:00 +0100

    game render improved, mouse coordinates simplified

--- game.js
+++ game.js
@@ -14,8 +14,29 @@ Object is a class that
 	private:
 */
 
-var Game = {};
-var p = Game;
+var Game = function(element) {
+	var self = this;
+	this.display = new Display(element);
+	this.keyboard = new Keyboard(element);
+	this.mouse = new Mouse(element);
+	this.timer = new Timer(function(delta) {
+		self.display.resize();
+		self.logic(delta);
+		self.keyboard.done();
+		self.display.context.clearRect(0, 0, self.display.canvas.width, self.display.canvas.height);
+		self.render();
+	});
+	this.display.resolution(1440, 900);
+};
+var p = Game.prototype;
+
+p.start = function() {
+	this.timer.start();
+}
+
+p.stop = function() {
+	this.timer.stop();
+}
 
 // Exports
 window.Game = Game;
